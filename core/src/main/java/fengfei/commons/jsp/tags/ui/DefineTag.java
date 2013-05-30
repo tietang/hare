671ac664cbc7da1abca0902bfdadca797be64a1c
Tietang Wang <wtt.2012@gmail.com>
2013-05-30 17:17:27 +0800

    fixed bug <script tag>

--- core/src/main/java/fengfei/commons/jsp/tags/ui/DefineTag.java
+++ core/src/main/java/fengfei/commons/jsp/tags/ui/DefineTag.java
@@ -10,30 +10,32 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class DefineTag extends SimpleTagSupport {
 
-	public final static String CONTEXT_KEY = "HARE_DEFINE_TAG_";
-	private String name;
+    public final static String CONTEXT_KEY = "HARE_DEFINE_TAG_";
+    private String name;
 
-	@Override
-	public void doTag() throws JspException, IOException {
-		PageContext pageContext = (PageContext) getJspContext();
-		try {
-			pageContext.getOut().clear();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		ServletRequest request = pageContext.getRequest();
-		request.setAttribute(CONTEXT_KEY + name, this);
-	}
+    @Override
+    public void doTag() throws JspException, IOException {
+        PageContext pageContext = (PageContext) getJspContext();
+        try {
+            pageContext.getOut().clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ServletRequest request = pageContext.getRequest();
+        request.setAttribute(CONTEXT_KEY + name, this);
+    
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void invoke(Writer out) throws IOException, JspException {
-		getJspBody().invoke(out);
-	}
+    public void invoke(Writer out) throws IOException, JspException {
+
+        getJspBody().invoke(out);
+    }
 }
--- core/src/main/java/fengfei/commons/jsp/tags/ui/ScriptTag.java
+++ core/src/main/java/fengfei/commons/jsp/tags/ui/ScriptTag.java
@@ -16,35 +16,35 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
  */
 public class ScriptTag extends SimpleTagSupport implements DynamicAttributes {
 
-	private Map<String, Object> attributes = new HashMap<String, Object>();
-	HtmlElement htmlElement = new HtmlElement("script");
-	// <link href="index.css" media="screen" rel="stylesheet" type="text/css" />
-	private String src;
+    private Map<String, Object> attributes = new HashMap<String, Object>();
+    HtmlElement htmlElement = new HtmlElement("script");
+    // <link href="index.css" media="screen" rel="stylesheet" type="text/css" />
+    private String src;
 
-	public ScriptTag() {
-		super();
-		htmlElement.addAttribute("type", "text/javascript");
+    public ScriptTag() {
+        super();
+        htmlElement.addAttribute("type", "text/javascript");
 
-	}
+    }
 
-	@Override
-	public void doTag() throws JspException, IOException {
-		PageContext pageContext = (PageContext) getJspContext();
+    @Override
+    public void doTag() throws JspException, IOException {
+        PageContext pageContext = (PageContext) getJspContext();
 
-		String path = pageContext.getServletContext().getContextPath();
-		htmlElement.addAttribute("href", path + src);
-		getJspContext().getOut().write(htmlElement.toHtml());
-	}
+        String path = pageContext.getServletContext().getContextPath();
+        htmlElement.addAttribute("src", path + src);
+        getJspContext().getOut().write(htmlElement.toHtml());
+    }
 
-	@Override
-	public void setDynamicAttribute(String uri, String localName, Object value)
-			throws JspException {
-		attributes.put(localName, value);
-		htmlElement.addAttribute(localName, value.toString());
+    @Override
+    public void setDynamicAttribute(String uri, String localName, Object value)
+        throws JspException {
+        attributes.put(localName, value);
+        htmlElement.addAttribute(localName, value.toString());
 
-	}
+    }
 
-	public void setSrc(String src) {
-		this.src = src;
-	}
+    public void setSrc(String src) {
+        this.src = src;
+    }
 }
